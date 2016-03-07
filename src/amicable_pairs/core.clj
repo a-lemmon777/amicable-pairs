71804d5ebbfbbd3bba2c05524c1410dc6b6954a3
Aaron Lemmon <lemmo031@morris.umn.edu>
2016-03-07 17:42:38 -0600

    Added comments

--- src/amicable_pairs/core.clj
+++ src/amicable_pairs/core.clj
@@ -1,8 +1,9 @@
 (ns amicable-pairs.core)
 (require '[clojure.core.reducers :as r])
 
-(defn handle-perfect-square
-  ""
+(defn sum-complementary-divisors
+  "If a divisor is a perfect square, returns the divisor.
+  Otherwise returns the sum of the two complementary divisors."
   [num divisor]
   (let [dividend (/ num divisor)]
     (if (= divisor dividend)
@@ -10,40 +11,25 @@
       (+ divisor dividend))))
 
 (defn sum-divisors
-  ""
+  "Sums up all divisors for a number."
   [num]
   (let [sqrt (Math/sqrt num)
         lower-divisibles (r/filter #(zero? (mod num %)) (range 1 (inc (int sqrt))))
-        last-piece (- (r/fold + (r/map #(handle-perfect-square num %) lower-divisibles)) num)]
+        last-piece (- (r/fold + (r/map #(sum-complementary-divisors num %) lower-divisibles)) num)]
     last-piece))
 
 (defn amicable-pair?
-  ""
+  "Checks if two numbers are amicable (the sum of each number's divisors is equal to the other number)."
   [pair]
   (if (>= (first pair) (second pair))
     false
     (= (sum-divisors (second pair)) (first pair))))
 
 (defn find-amicable-pairs
-  ""
+  "Returns the first num-pairs of amicable pairs."
   [num-pairs]
 (let [pairs (pmap #(vector % (sum-divisors %)) (range))]
   (r/foldcat (r/take num-pairs (r/filter #(amicable-pair? %) pairs)))))
 
 (time (find-amicable-pairs 20)
 )
-
-;; 7  1.5  .2  .2 .173 .15
-;; 8
-;; 9
-;; 10  20  1   1  1.1 .8
-;; 11
-;; 12
-;; 13
-;; 14  62 2.4  2.3 2.7  1.6
-;; 15
-;; 16
-;; 17
-;; 18
-;; 19
-;; 20  5.5 minutes  4.2  4.0 5.0 3.1
